5faf4572841bbcd039f44592a0f5dbdb00fee191
Sean@digifinex.org <pengdarong@digifinex.org>
2021-11-29 18:47:33 +0800

    improve

--- auto_getter.go
+++ auto_getter.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/antchfx/htmlquery"
-	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/v2"
+	"github.com/gocolly/colly/v2/proxy"
 )
 
 type AutoProxyGetter interface {
@@ -16,14 +18,18 @@ type AutoProxyGetter interface {
 	CrawlProxy(url string) // Crawl available proxies from the Internet
 }
 
-type DefaultAutoProxyGetter struct {
+//
+type TimerAutoProxyGetter struct {
 	DefaultProxyGetter
 	interval int // How many seconds between crawling proxies
 }
 
-// 先加url,再爬proxy
+type MiniThresholdAutoProxyGetter struct {
+	DefaultProxyGetter
+	min int // Actively crawl when the proxy is lower than this value
+}
 
-func (p *DefaultAutoProxyGetter) CrawlProxy(url string) {
+func (p *TimerAutoProxyGetter) CrawlProxy(url string) {
 	timeTickerChan := time.Tick(time.Second * time.Duration(p.interval))
 	// init collyCollector
 	c := colly.NewCollector(
@@ -44,6 +50,19 @@ func (p *DefaultAutoProxyGetter) CrawlProxy(url string) {
 		ExpectContinueTimeout: 10 * time.Second,
 	})
 	for {
+		if p.proxies != nil && len(p.proxies) != 0 {
+			ipPort := []string{}
+			for _, v := range p.proxies {
+				ipPort = append(ipPort, "socks5://"+v)
+			}
+			rp, err := proxy.RoundRobinProxySwitcher(ipPort...)
+			if err != nil {
+				log.Fatal(err)
+			}
+			// Set proxy ip, here is polling
+			c.SetProxyFunc(rp)
+		}
+
 		proxyList := []string{}
 		c.OnRequest(func(r *colly.Request) {
 			// fmt.Println("Visiting", r.URL)
@@ -63,11 +82,16 @@ func (p *DefaultAutoProxyGetter) CrawlProxy(url string) {
 				proxy := htmlquery.InnerText(addr) + ":" + htmlquery.InnerText(port)
 				proxyList = append(proxyList, proxy)
 			}
+			pageList := make(map[string]int)
 			// nextPage
-			// pages := htmlquery.Find(doc, `//*[@id="listnav"]//a/@href`)
-			// for _, page := range pages {
-			// 	fmt.Println("Page:", htmlquery.InnerText(page))
-			// }
+			pages := htmlquery.Find(doc, `//*[@id="listnav"]//a/@href`)
+			for _, page := range pages {
+				_, ok := pageList[htmlquery.InnerText(page)]
+				if !ok {
+					pageList[htmlquery.InnerText(page)] = 1
+					go p.CrawlProxy(url + htmlquery.InnerText(page))
+				}
+			}
 		})
 
 		// c.OnHTML()
--- auto_getter_test.go
+++ auto_getter_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestAutoCrawl(t *testing.T) {
-	p := &DefaultAutoProxyGetter{
+	p := &TimerAutoProxyGetter{
 		interval: 10,
 	}
 	p.CrawlProxy("http://www.ip3366.net/")
